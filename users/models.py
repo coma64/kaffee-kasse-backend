05104fc68ecfb88f28bdbff69cfdc9d61326faa9
Tristan Zaton <50082122+coma64@users.noreply.github.com>
2021-05-01 20:46:01 +0200

    Add user profile model and migrations

--- users/models.py
+++ users/models.py
@@ -1,7 +1,22 @@
 from django.contrib.auth.models import User
+from django.db.models import (
+    CASCADE,
+    BooleanField,
+    DecimalField,
+    Model,
+    OneToOneField,
+    TextField,
+)
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
+
+
+class Profile(Model):
+    user = OneToOneField(User, on_delete=CASCADE)
+    is_freeloader = BooleanField(default=False)
+    balance = DecimalField(max_digits=15, decimal_places=2, default=0)
+    bio = TextField(default='')
 
 
 @receiver(post_save, sender=User)
@@ -10,3 +25,16 @@ def create_auth_token(
 ) -> None:
     if created:
         Token.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(
+    sender, instance: User = None, created: bool = False, **kwargs
+) -> None:
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance: User, **kwargs) -> None:
+    instance.profile.save()
