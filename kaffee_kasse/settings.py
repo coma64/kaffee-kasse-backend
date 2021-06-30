75cde6f4d418fd647c0200343e7eea40997c8253
coma64 <zaton.tristan@gmail.com>
2021-06-30 14:42:36 +0200

    Disable django debug

--- kaffee_kasse/settings.py
+++ kaffee_kasse/settings.py
@@ -22,13 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = (
     'django-insecure-m8$gcet+#_l75p%tev0@b$-56fdn3^9pghx#&2%m+7$s8uei#q'
-    if DEBUG
-    else environ['DJANGO_SECRET_KEY']
 )
 
 ALLOWED_HOSTS: List[str] = ['backend', 'localhost']
